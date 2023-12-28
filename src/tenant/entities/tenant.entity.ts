e7b733ffc2843409992a7e6dea297c6aa5391741
Sai Hlaing Lu <saihlainglu@Sais-MacBook-Pro.local>
2023-12-28 13:51:27 +0630

    disable entity synchronize

--- src/tenant/entities/tenant.entity.ts
+++ src/tenant/entities/tenant.entity.ts
@@ -1,10 +1,13 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('tenants')
+@Entity('tenants', { synchronize: false })
 export class TenantEntity {
   @PrimaryGeneratedColumn('increment')
   id: number;
 
   @Column()
   db_name: string;
+
+  @Column({ nullable: true })
+  test: string;
 }
